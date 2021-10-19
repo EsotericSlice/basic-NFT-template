d97733e51ad34811448dd0d690281164e356894a
EsotericSlice <91757381+EsotericSlice@users.noreply.github.com>
2021-10-19 14:17:48 -0400

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,21 @@
+const layersOrder = [
+    { name: '', number: 0 },
+    { name: '', number: 0 },
+    { name: '', number: 0 },
+    { name: '', number: 0 },
+];
+  
+const format = {
+    width: 1000,
+    height: 1000
+};
+
+const rarity = [
+    { key: "", val: "original" },
+    { key: "_r", val: "rare" },
+    { key: "_sr", val: "super rare" },
+];
+
+const defaultEdition = 0;
+
+module.exports = { layersOrder, format, rarity, defaultEdition };
